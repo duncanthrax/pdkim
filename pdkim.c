14938d5b9ab408fab8edfba53cbf2d3b90f1ba0b
root <root@whizbang.eurescom.eu>
2009-10-29 11:01:09 +0100

    Fix insufficient buffer space when relaxing one-word headers.

--- pdkim.c
+++ pdkim.c
@@ -366,7 +366,7 @@ char *pdkim_relax_header (char *header, int crlf) {
   int seen_wsp = 0;
   char *p = header;
   char *q;
-  char *relaxed = malloc(strlen(header));
+  char *relaxed = malloc(strlen(header)+2);
   if (relaxed == NULL) return NULL;
   q = relaxed;
   while (*p != '\0') {
